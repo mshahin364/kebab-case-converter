0549060e83609882037facbe37a8ca23922114ef
Shahinur Islam <mshahin364@gmail.com>
2020-03-15 18:37:17 +0600

    Updates

---
+++ precache-manifest.0899e1babbc00998a15ec90d09aec858.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d078b75bdcaf7fa1f1205585b591bf9",
+    "url": "/kebab-case-converter/index.html"
+  },
+  {
+    "revision": "f0c22a8b597f03fdcf59",
+    "url": "/kebab-case-converter/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "37d45ba91cb2d06e2982",
+    "url": "/kebab-case-converter/static/js/2.67cb36ce.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/kebab-case-converter/static/js/2.67cb36ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0c22a8b597f03fdcf59",
+    "url": "/kebab-case-converter/static/js/main.682a2680.chunk.js"
+  },
+  {
+    "revision": "b94f04fc7e82f48e2df5",
+    "url": "/kebab-case-converter/static/js/runtime-main.c2e958ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kebab-case-converter/static/media/logo.5d5d9eef.svg"
+  }
+]);
