2623865c4de01787dd8416089a8c6f3d5334d5b4
Shahinur Islam <mshahin364@gmail.com>
2020-03-16 15:03:20 +0600

    kebab-case-converter: fontawesome & github link added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import KebabCaseConverter from "./KebabCaseConverter";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import "./App.css";
 
 function App() {
   return (
     <div className="container">
+      <div className="header">
+        <span className="github-link">
+          <a href="https://github.com/mshahin364/kebab-case-converter" className="href"><FontAwesomeIcon icon={faGithub} /></a>
+        </span>
+      </div>
+      
       <KebabCaseConverter />
       <div className="author-container">
         <span className="author">by mshahin364</span>
